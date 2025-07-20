bb7dad054687f53698a163b3ca62a49d336655c7
kbarker0201 <kbarker0201@gmail.com>
2025-07-20 16:01:08 -0500

    final changes

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,23 +24,29 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+// console.log("Welcome to the bootcamp
 
 // What’s Wrong?
-
+// The error in this example is a syntax error - the ending " and ) are missing
+// Correction:
+console.log("Welcome to the bootcamp");
 
 // Program B
 // Description:
-// This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
+// This code attempts to multiply each number in an array by 2 and display the results. 
+// However, it crashes at runtime.
 
 let numbers = [2, 4, "eight"];
 for (let i = 0; i < numbers.length; i++) {
-  let doubled = numbers[i] * 2;
+  let doubled = Number(numbers[i]) * 2;
   console.log(doubled);
 }
-
 // What’s Wrong?
-
+// The error here is a runtime error, as it does not execute and crashes at runtime. 
+// This is happening because the third item in the array is a string and not a number,
+// but the loop tries to perform arithmetic on each item in the array. The fix here 
+// would be to make sure that each item of the array is converted to a number before 
+// the multiplication occurs using the Number() function. 
 
 
 // Program C (Logic Error)
@@ -49,7 +55,7 @@ for (let i = 0; i < numbers.length; i++) {
 
 function isPrime(num) {
   if (num < 2) return false;
-  for (let i = 2; i < num; i++) {
+  for (let i = 2; i <= num; i++) {
     if (num % i === 0) {
       return true;  // Supposed to indicate num is NOT prime
     }
@@ -60,3 +66,6 @@ function isPrime(num) {
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+// This is a logic error - the program still runs but the output is the opposite of 
+// what we expect. The error stems from the for loop - rather than saying that it should
+// run while i < num, it should be i <= num. 
